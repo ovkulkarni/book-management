c77a780ca3763b2cb5824190f504084885a0171f
Omkar Kulkarni <2019okulkarn@tjhsst.edu>
2016-08-26 18:00:27 -0400

    update config

--- config/prod.py
+++ config/prod.py
@@ -4,7 +4,7 @@ APP_NAME = "CMWRC Book Store"
 
 SECRET_KEY = os.getenv("SECRET_KEY", os.urandom(32))
 
-DB_PATH = "/home/bookstore/bookstore.db"
+DB_PATH = "/var/www/bookstore/bookstore.db"
 
 DISPLAY_DEBUG_INFO = False
 ENVIRONMENT = "prod"
