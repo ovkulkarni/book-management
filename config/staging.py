8d919fdb3a736ec85393c51564026b2e40dd11c8
Omkar Kulkarni <2019okulkarn@tjhsst.edu>
2016-08-28 17:46:19 -0400

    Revert "update config"
    This reverts commit 769802a0d24a4c65dfeef18bf5e8f47cbe9429c5.

--- config/staging.py
+++ config/staging.py
@@ -1,6 +1,6 @@
 APP_NAME = "CMWRC Book Store"
 SECRET_KEY = "key"
-DB_PATH = "/home/bookstore/bookstore-test.db"
+DB_PATH = "/home/okulkarni/book-management/bookstore.db"
 DISPLAY_DEBUG_INFO = True
 ENVIRONMENT = "staging"
 TEMPLATES_AUTO_RELOAD=True
