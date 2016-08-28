769802a0d24a4c65dfeef18bf5e8f47cbe9429c5
Omkar Kulkarni <2019okulkarn@tjhsst.edu>
2016-08-28 17:15:45 -0400

    update config

--- config/staging.py
+++ config/staging.py
@@ -1,6 +1,6 @@
 APP_NAME = "CMWRC Book Store"
 SECRET_KEY = "key"
-DB_PATH = "/home/okulkarni/book-management/bookstore.db"
+DB_PATH = "/home/bookstore/bookstore-test.db"
 DISPLAY_DEBUG_INFO = True
 ENVIRONMENT = "staging"
 TEMPLATES_AUTO_RELOAD=True
